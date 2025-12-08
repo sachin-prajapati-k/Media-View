d8faceb65fe579f526bee0373c42f9576e35cc2f
sachin <sdev77577@gmail.com>
2025-12-08 20:27:22 +0530

    first code

---
+++ Components/Democomponent.tsx
@@ -0,0 +1,9 @@
+ const Democomponent=()=>{
+    return(
+      <>
+      <h1>hello</h1>
+      </>
+    )
+}
+
+export default Democomponent;
--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name="sachin"
+  const currentYear= new Date().getFullYear();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello {name}</h1>
+     <h>CurrentYear is : {currentYear}</h>
     </div>
   );
 }
