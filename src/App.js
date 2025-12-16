53b07d04281acbc27dabab5fd596bc6b8541bde6
sachin <sdev77577@gmail.com>
2025-12-16 20:31:10 +0530

    sfsfas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import ProductList from "./Components New/ProductCard/ProductList";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Components/PortfolioCart.module.css";
 
 function App() {
   return (
--- src/Components New/ProductCard/ProductCard.tsx
+++ src/Components New/ProductCard/ProductCard.tsx
@@ -4,44 +4,47 @@ import { productsTypes } from "../../types";
 function ProductCard({ product }: { product: productsTypes }) {
   return (
     <>
-      <div className="card m-3" style={{ width: "15rem", height: "20rem" }}>
-        <div className="product-img">
+      <div
+        className="card m-2 text-start "
+        style={{ width: "15rem", height: "20rem", overflow: "hidden" }}
+      >
+        <div className="product-img mb-0">
           <img
-            className="card-img-top p-1"
+            className="card-img"
             src={product.product_img || "#"}
             alt={product.product_name}
-            style={{ width: "100%", height: "100px", borderRadius: "10px" }}
+            style={{ width: "100%", height: "100px", borderRadius: "5px" }}
           />
         </div>
-        <div className="text-start">
-          <div className="card-body ">
-            <div className="card-title mb-2">
-              <h2 className="h5">{product.product_name}</h2>
-            </div>
-            <div className="mb-2">
-              <p className="card-text">{product.product_description}</p>
-            </div>
 
-            <div className="card-subtitle mb-2 text-muted">
-              <p className="mb-1">Rating : ⭐ {product.product_rating} / 5.0</p>
-            </div>
-            <div className="mb-2">
-              <p className="m-0">
-                <span className="fw-bold">
-                  {" "}
-                  Price : ₹{product.product_price}{" "}
-                </span>
-                <span
-                  className="text-danger fw-bold"
-                  style={{ textDecoration: "line-through" }}
-                >
-                  {product.sale_price}
-                </span>
-              </p>
-            </div>
-            <div className="">in stock or not</div>
+        <div className="m-2 mb-0 ">
+          <p className="fs-6 fw-bold">{product.product_name}</p>
+          <div className="text-truncate">
+            <p
+              className="card-text"
+              style={{ fontSize: "14px", lineHeight: "20px" }}
+            >
+              {product.product_description}
+            </p>
           </div>
-          <ClickEvent />
+
+          <div className="text-muted " style={{ fontSize: "12px" }}>
+            <p className="mb-1">Rating : ⭐ {product.product_rating} / 5.0</p>
+          </div>
+
+          <p className="mb-0">
+            <span className="fw-bold">Price : ₹{product.product_price} </span>
+            <span
+              className="text-danger fw-bold"
+              style={{ textDecoration: "line-through" }}
+            >
+              {product.sale_price}
+            </span>
+          </p>
+          <div style={{ position: "absolute", bottom: 4 }}>
+            <ClickEvent />
+          </div>
+          {/* <div className="">in stock or not</div> */}
         </div>
       </div>
     </>
