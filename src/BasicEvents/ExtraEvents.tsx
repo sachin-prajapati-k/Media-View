955cd315cf9681a2f8d750579c95ee53b37ac5c4
sachin <sdev77577@gmail.com>
2025-12-17 19:01:02 +0530

    sdfasfd

--- src/BasicEvents/ExtraEvents.tsx
+++ src/BasicEvents/ExtraEvents.tsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import Button from "react-bootstrap/esm/Button";
+
 export const ExtraEvents = () => {
+  const [count, setCount] = useState(0);
   const handleClick = () => {
     alert("hello");
   };
+
+  const onMouseEnter = () => {
+    prompt("hello");
+  };
+  const handleCountPlus = () => {
+    setCount(count + 1);
+  };
+  const handleCountMinus = () => {
+    setCount(count - 1);
+  };
   return (
     <>
-      <p onMouseOver={handleClick}>hello click me</p>
+      <h3>{count}</h3>
+
+      <Button className="bg-primary" onClick={handleCountPlus}>
+        inCrement
+      </Button>
+      <Button className="bg-secondary" onClick={handleCountMinus}>
+        inCrement
+      </Button>
+      <Button className="bg-info" onClick={() => setCount(0)}>
+        reset
+      </Button>
     </>
   );
 };
